d67f4cdd14920c1c59ecb884c4ad3714f2c08383
Marcin Czekaj <mar.czekaj11@gmail.com>
2024-07-19 12:02:17 +0200

    Added lambda and functor example

--- WorkerExample.cpp
+++ WorkerExample.cpp
@@ -46,8 +46,17 @@ namespace C
     }
 }
 
+struct X
+{
+    void operator()()
+    {
+        std::cout << "Functor::X()\n";
+    }
+};
+
 void WorkerExample::Update()
 {
+    X x;
     WorkerManager<Worker> manager;
 
     manager.UpdateCustomerInfo.REGISTER_FUNC(func);
@@ -57,6 +66,9 @@ void WorkerExample::Update()
     manager.UpdateCustomerInfo.REGISTER_FUNC(B::f);
     manager.UpdateCustomerInfo.REGISTER_FUNC(C::f);
     manager.retUpdate.REGISTER_FUNC(getMul);
+    auto lambda = []() {std::cout << "Lambda Test\n"; };
+    manager.UpdateCustomerInfo.REGISTER_FUNC(lambda);
+    manager.UpdateCustomerInfo.REGISTER_METHOD(&x, &X::operator());
     Worker worker1(1);
 
     worker1.RegisterAtWork(manager);
@@ -73,7 +85,9 @@ void WorkerExample::Update()
 
     manager.UpdateCustomerInfo.UNREGISTER_FUNC(func);
     manager.UpdateCustomerInfo.UNREGISTER_FUNC(B::f);
-
+    manager.UpdateCustomerInfo.UNREGISTER_FUNC(lambda);
+    manager.UpdateCustomerInfo.UNREGISTER_METHOD(&x, &X::operator());
+        
     worker2.UnRegisterFromWork(manager);
 
     std::cout << "---------------------------" << std::endl;
